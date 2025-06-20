35ac4bf6d7cd25caef8b7f87f5f9c77bf4f15085
manjunath-sp <manjunath.sp@hotmail.com>
2025-06-20 14:37:23 +1000

    Create Screenshot 2025-06-20 142005.py

---
+++ Screenshot 2025-06-20 142005.py
@@ -0,0 +1,12 @@
+def solution(digits):
+    from collections import Counter
+
+    count = Counter(digits)
+
+    # Make each count even
+    count['1'] -= count['1'] % 2
+    count['2'] -= count['2'] % 2
+
+    # Rebuild the string with sorted digits in descending order for maximum value
+    result = '2' * count['2'] + '1' * count['1']
+    return result
