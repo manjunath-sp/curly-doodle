1d50181d86d93a37af26fe8ed4184b5c70fefe27
manjunath-sp <manjunath.sp@hotmail.com>
2025-06-23 13:23:22 +1000

    removed

--- Screenshot 2025-06-20 142225.py
+++
@@ -1,9 +0,0 @@
-def solution(string):
-    frequency = [0] * 26
-    for character in string:
-        frequency[ord(character) - ord('a')] += 1
-
-    max_count = max(frequency)
-    for i in range(26):
-        if frequency[i] == max_count:
-            return chr(i + ord('a'))
